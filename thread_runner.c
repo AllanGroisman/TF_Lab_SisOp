ea34d197699ef05ec43fa4f878767be4ab935185
Allan Groisman <allan.groisman@edu.pucrs.br>
2025-06-20 18:49:28 -0300

    a principio finalizado sem deadline.

--- thread_runner.c
+++ thread_runner.c
@@ -6,43 +6,63 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-#include <time.h>
+#include <sys/time.h>
+#include <semaphore.h>  // necessário para sem_t
 
 #define tam_alfabeto 26  // Limite de A-Z
 
-//buffer e tamanho dele
+// buffer e tamanho dele
 char *buffer;
 int tam_buffer;
-//comeca apontando na posicao 0
+// começa apontando na posição 0
 int pos_buffer = 0;
 
 int exec_count[tam_alfabeto];
-pthread_mutex_t mutex;
+// semáforo binário para controlar acesso ao buffer
+sem_t semaforo;
 
 typedef struct {
     int id;
     char symbol;
 } ThreadArg;
 
+// função para delay em microssegundos (usando busy-wait com gettimeofday)
+void delay_us(unsigned long delay)
+{
+    struct timeval time1, time2;
+    long long timel1, timel2;
+
+    gettimeofday(&time1, 0);
+
+    timel1 = time1.tv_sec * 1000000LL;
+    timel1 += time1.tv_usec;
+
+    do {
+        gettimeofday(&time2, 0);
+        timel2 = time2.tv_sec * 1000000LL;
+        timel2 += time2.tv_usec;
+    } while (timel2 - timel1 < delay);
+}
+
 void* thread_func(void* arg) {
     ThreadArg *targ = (ThreadArg*) arg;
 
-    struct timespec delay;
-    delay.tv_sec = 0;
-    delay.tv_nsec = 1000000; // 1ms
-
     while (1) {
-        nanosleep(&delay, NULL);
+        // delay de 1ms
+        delay_us(1000);
 
-        pthread_mutex_lock(&mutex);
+        // entra na seção crítica usando o semáforo
+        sem_wait(&semaforo);
+        //verifica se acabou o buffer
         if (pos_buffer >= tam_buffer) {
-            pthread_mutex_unlock(&mutex);
+            sem_post(&semaforo);
             break;
         }
-
+        //senao grava a letra correspondente no buffer, atualiza a posicao do ponteiro e aumenta o contador daquela letra 
         buffer[pos_buffer++] = targ->symbol;
         exec_count[targ->id]++;
-        pthread_mutex_unlock(&mutex);
+        // libera a seção crítica
+        sem_post(&semaforo);
     }
 
     return NULL;
@@ -56,35 +76,35 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //pega os parametros
+    // pega os parâmetros
     int num_threads = atoi(argv[1]);
     int tam_buffer_arg = atoi(argv[2]);
     char *politica_str = argv[3];
     int prioridade = atoi(argv[4]);
 
-    //se passar o limite de threads
+    // se passar o limite de threads
     if (num_threads > tam_alfabeto) {
         fprintf(stderr, "Máximo de %d threads suportado. Tente novamente\n", tam_alfabeto);
         return 1;
     }
 
-    //tam do buffer em bytes 
+    // tam do buffer em bytes 
     tam_buffer = tam_buffer_arg * 1024;
-    //aloca o muffer
+    // aloca o buffer
     buffer = malloc(tam_buffer);
     if (!buffer) {
         perror("Erro ao alocar buffer");
         return 1;
     }
 
-    //cria o mutex
-    pthread_mutex_init(&mutex, NULL);
+    // inicializa o semáforo binário (1 = recurso livre)
+    sem_init(&semaforo, 0, 1);
 
-    //cria os vetores de threads no tamanho maximo possivel
+    // cria os vetores de threads no tamanho máximo possível
     pthread_t threads[tam_alfabeto];
     ThreadArg args[tam_alfabeto];
 
-    //escolher a politica de acorda com a entrada
+    // escolher a política de acordo com a entrada
     int politica;
     if (strcmp(politica_str, "SCHED_FIFO") == 0) politica = SCHED_FIFO;
     else if (strcmp(politica_str, "SCHED_RR") == 0) politica = SCHED_RR;
@@ -92,61 +112,68 @@ int main(int argc, char *argv[]) {
     else if (strcmp(politica_str, "SCHED_BATCH") == 0) politica = SCHED_BATCH;
     else if (strcmp(politica_str, "SCHED_IDLE") == 0) politica = SCHED_IDLE;
     else {
-        fprintf(stderr, "Política desconhecida.Tente novamente.\n");
+        fprintf(stderr, "Política desconhecida. Tente novamente.\n");
         return 1;
     }
 
-    //pra cada thread
+    // pra cada thread
     for (int i = 0; i < num_threads; i++) {
         
-        //endereça qual é o id e o caracter dele
+        // endereça qual é o id e o caractere dele
         args[i].id = i;
         args[i].symbol = 'A' + i;
 
-        //inicia a thread
+        // inicia a thread
         pthread_attr_t attr;
         pthread_attr_init(&attr);
 
         struct sched_param param;
         param.sched_priority = prioridade;
 
-        //atribui a politica escolhida
+        // atribui a política escolhida
         pthread_attr_setschedpolicy(&attr, politica);
 
-        //salva a prioridade
+        // salva a prioridade
         pthread_attr_setschedparam(&attr, &param);
 
-        //faz com que force a utilizacao da politica independente do pai
+        // faz com que force a utilização da política independente do pai
         pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
         
-        //caso de erro
+        // caso de erro
         if (pthread_create(&threads[i], &attr, thread_func, &args[i]) != 0) {
-            perror("Erro ao criar thread.Tente novamente.");
+            perror("Erro ao criar thread. Tente novamente.");
             return 1;
         }
 
-        //libera os recursos pra criacao da thread
+        // libera os recursos pra criação da thread
         pthread_attr_destroy(&attr);
     }
 
-    // comeca todas elas
+    // espera todas elas terminarem
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 
+    // imprime o buffer resultante
     fwrite(buffer, 1, tam_buffer, stdout);
-    printf("\n");
+    printf("\n\n");
 
+
+    // imprime as letras utilizadas
     for (int i = 0; i < num_threads; i++) {
         printf("%c", 'A' + i);
     }
     printf("\n");
 
+    // imprime quantas vezes cada thread escreveu
     for (int i = 0; i < num_threads; i++) {
         printf("%c = %d\n", 'A' + i, exec_count[i]);
     }
 
+    //libera o buffer
     free(buffer);
-    pthread_mutex_destroy(&mutex);
+
+    // destrói o semáforo
+    sem_destroy(&semaforo);
     return 0;
 }
